0079b1f2ba398579254bb6259a118fcb51cecef7
saqibscriza <saqib@scriza.in>
2025-06-14 16:10:12 +0530

    14/06/25 saqib

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google"; 
+import Navbar from './navbar'
+import Footer from './footer'
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.css'
 
@@ -24,7 +26,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
